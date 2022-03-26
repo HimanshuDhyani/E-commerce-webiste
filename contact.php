b0b14fa5f60a25a803a3c9241b6c4e8a07b5078d
himanshu <himanshudhyani231@gmail.com>
2022-03-26 16:54:30 +0530

    hjhj

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Contact Form</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+     <div class="contact-form">
+        <h2>CONTACT US</h2>
+        <form method="post" action="">
+            <input type="text" name="name" placeholder="Your Name" required>
+            <input type="text" name="phone" placeholder="Phone No" required>
+            <input type="email" name="email" placeholder="Your Email" required>
+            <textarea name="message" id="#" cols="30" rows="10" placeholder="Your Message" required></textarea>
+            <input type="submit" name="submit" value="Send Message" class="submit-btn">
+        </form>
+     </div>
+
+    </body>
+</html>
